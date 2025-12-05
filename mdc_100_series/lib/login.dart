c0d71c1fa916b9aaaf7fb201a0172c7e5fc05c61
gerardo-cossio <gcossio03@gmail.com>
2025-12-04 18:59:01 -0600

    commit to merge main

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -1,18 +1,6 @@
-// Copyright 2018-present the Flutter authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:shrine/colors.dart';
 import 'register_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -40,7 +28,7 @@ class _LoginPageState extends State<LoginPage> {
                 Image.asset('assets/mammal.png', width: 150, height: 150),
                 const SizedBox(height: 50.0),
                 const Text(
-                'GorillaPrintShop',
+                'Gorilla Printer Shop',
                 style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
                 textAlign: TextAlign.center
                 ),
@@ -108,6 +96,9 @@ class _LoginPageState extends State<LoginPage> {
                   },
                   style: ElevatedButton.styleFrom(
                     elevation: 5.0,
+                    foregroundColor: coconutWhite,
+                    backgroundColor: inkBlack,
+                    
                   ),
                 ),
                 // TODO: Add an elevation to NEXT (103)
@@ -119,6 +110,8 @@ class _LoginPageState extends State<LoginPage> {
                   },
                   style: ElevatedButton.styleFrom(
                     elevation: 5.0,
+                    foregroundColor: inkBlack,
+                    backgroundColor: bananaYellow,
                   ),
                 ),
               ],

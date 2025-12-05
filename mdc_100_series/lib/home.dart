d5b851d1c94b7c4646536e1958eedd7cfdcf5595
gerardo-cossio <gcossio03@gmail.com>
2025-12-04 19:29:03 -0600

    fixes hom.dart commit issues

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -1,23 +1,25 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'login.dart';
-import 'services/database_printers.dart';
-import 'package:shrine/colors.dart';
 
 import 'model/product.dart';
-import 'cart.dart';
 import 'model/products_repository.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final DatabaseService _databaseService = DatabaseService.instance;
 
   List<Card> _buildGridCards(BuildContext context) {
     List<Product> products = ProductsRepository.loadProducts(Category.all);
@@ -33,7 +35,9 @@ class _HomePageState extends State<HomePage> {
     return products.map((product) {
       return Card(
         clipBehavior: Clip.antiAlias,
+        // TODO: Adjust card heights (103)
         child: Column(
+          // TODO: Center items on the card (103)
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             AspectRatio(
@@ -48,23 +52,20 @@ class _HomePageState extends State<HomePage> {
               child: Padding(
                 padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
                 child: Column(
-
                   // TODO: Align labels to the bottom and center (103)
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   // TODO: Change innermost Column (103)
                   children: <Widget>[
+                    // TODO: Handle overflowing labels (103)
                     Text(
                       product.name,
                       style: theme.textTheme.titleLarge,
-                      softWrap: false,
-                      overflow: TextOverflow.ellipsis,
                       maxLines: 1,
                     ),
                     const SizedBox(height: 8.0),
                     Text(
                       formatter.format(product.price),
-                      style: theme.textTheme.bodyLarge,
+                      style: theme.textTheme.titleSmall,
                     ),
                   ],
                 ),
@@ -76,92 +77,45 @@ class _HomePageState extends State<HomePage> {
     }).toList();
   }
 
+  // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
+    // TODO: Return an AsymmetricView (104)
+    // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: gorillaGrey,
         leading: IconButton(
-
           icon: const Icon(
             Icons.menu,
             semanticLabel: 'menu',
-            color: coconutWhite,
           ),
           onPressed: () {},
         ),
-
-
-        title: Row( 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-            const Text('GorillaPrintShop', style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold, color: bananaYellow),),
-
-            IconButton(
-            icon: Image.asset('assets/mammal.png', width: 40, height: 40),
-          onPressed: () {
-            // TODO: Add open login (104)
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (BuildContext context) => LoginPage()),
-            );
-          },
-        ),
-
-      ],
-    ),
-
-    automaticallyImplyLeading: true,
-
-        
+        title: const Text('SHRINE'),
         actions: <Widget>[
           IconButton(
-
             icon: const Icon(
               Icons.search,
               semanticLabel: 'search',
-              color: coconutWhite,
             ),
             onPressed: () {},
           ),
-
-      
+          IconButton(
+            icon: const Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () {},
+          ),
         ],
       ),
-
       body: GridView.count(
         crossAxisCount: 2,
         padding: const EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
         children: _buildGridCards(context),
       ),
-      
       resizeToAvoidBottomInset: false,
-      floatingActionButton: _shoppingCartButton(),
-    );
-
-  }
-
-
-  Widget _shoppingCartButton() {
-    return FloatingActionButton(
-      onPressed: () {
-
-       Navigator.push(
-          context,
-          MaterialPageRoute(builder:(context) => const CartPage()),
-        );
-        // TODO: hook into DatabaseService here
-        
-      },
-      child: Image.asset(
-      'assets/shoppingcart.png',
-      width: 24,
-      height: 24,
-      ),
     );
   }
 }
-
